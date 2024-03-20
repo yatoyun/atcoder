c514807caf14bd7f94a0878ace056d8047c9ceb7
yatoyun <kaneko.yuta0813@gmail.com>
2024-03-20 18:00:01 +0900

    :white_check_mark: easy

---
+++ selections100/2-106-b.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+def main():
+    N = int(input())
+    ans = 0
+    for i in range(1, N+1, 2):
+        cnt = 0
+        for j in range(1, i+1):
+            if i % j == 0:
+                cnt += 1
+        if cnt == 8:
+            ans += 1
+    print(ans)
+
+if __name__ == "__main__":
+    main()
