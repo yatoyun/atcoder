a404e5624f21b7f2ba7b278c68e79af5d0ef00f8
yatoyun <kaneko.yuta0813@gmail.com>
2024-04-13 16:00:06 +0900

    :white_check_mark:

---
+++ selections100/95-abc149_b.py
@@ -0,0 +1,25 @@
+def main():
+    A, B, K = mapint()
+    if K > A:
+        K -= A
+        A = 0
+        B = max(0, B-K)
+    else:
+        A -= K
+    print(A, B)
+
+def ini(): return int(input())
+def mapint(): return map(int, input().split())
+def mapint0(): return map(lambda x: int(x)-1, input().split())
+def mapstr(): return input().split()
+def lint(): return list(map(int, input().split()))
+def lint0(): return list(map(lambda x: int(x)-1, input().split()))
+def lstr(): return list(input().rstrip())
+def errprint(*x): return None if atcenv else print(*x, file=sys.stderr) 
+
+if __name__=="__main__":
+    import sys, os
+    input = sys.stdin.readline
+    atcenv = os.environ.get("ATCODER", 0)
+
+    main()
