d60ad3760f7292a547c330cfdb2b3df8cbd2bf58
yatoyun <kaneko.yuta0813@gmail.com>
2024-04-14 17:52:24 +0900

    :white_check_mark: 5/22 minutes :brown_square:

---
+++ 334/d.py
@@ -0,0 +1,29 @@
+from bisect import bisect_left, bisect_right
+def main():
+    N, Q = mapint()
+    R = sorted(lint())
+
+    sumL = [R[0]]
+    for i in range(1, N):
+        sumL.append(sumL[-1]+R[i])
+    
+    for i in range(Q):
+        x = ini()
+        index = bisect_right(sumL, x)
+        print(index)
+
+def ini(): return int(input())
+def mapint(): return map(int, input().split())
+def mapint0(): return map(lambda x: int(x)-1, input().split())
+def mapstr(): return input().split()
+def lint(): return list(map(int, input().split()))
+def lint0(): return list(map(lambda x: int(x)-1, input().split()))
+def lstr(): return list(input().rstrip())
+def errprint(*x): return None if atcenv else print(*x, file=sys.stderr) 
+
+if __name__=="__main__":
+    import sys, os
+    input = sys.stdin.readline
+    atcenv = os.environ.get("ATCODER", 0)
+
+    main()
