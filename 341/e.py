7723fd07eedc28fc3193c8e76eb4b05e63743d5f
yatoyun <kaneko.yuta0813@gmail.com>
2024-05-02 13:38:38 +0900

    :rotating_light: need retry can't solve :droplet:

---
+++ 341/e.py
@@ -0,0 +1,110 @@
+#####segfunc#####
+def segfunc(x, y):
+    return min(x,y)
+#################
+
+#####ide_ele#####
+ide_ele = 2**31-1
+#################
+
+class SegTree:
+    """
+    init(init_val, ide_ele): 配列init_valで初期化 O(N)
+    update(k, x): k番目の値をxに更新 O(logN)
+    query(l, r): 区間[l, r)をsegfuncしたものを返す O(logN)
+    """
+    def __init__(self, init_val, segfunc, ide_ele):
+        """
+        init_val: 配列の初期値
+        segfunc: 区間にしたい操作
+        ide_ele: 単位元
+        n: 要素数
+        num: n以上の最小の2のべき乗
+        tree: セグメント木(1-index)
+        """
+        n = len(init_val)
+        self.segfunc = segfunc
+        self.ide_ele = ide_ele
+        self.num = 1 << (n - 1).bit_length()
+        self.tree = [ide_ele] * 2 * self.num
+        # 配列の値を葉にセット
+        for i in range(n):
+            self.tree[self.num + i] = init_val[i]
+        # 構築していく
+        for i in range(self.num - 1, 0, -1):
+            self.tree[i] = self.segfunc(self.tree[2 * i], self.tree[2 * i + 1])
+
+    def update(self, k, x):
+        """
+        k番目の値をxに更新
+        k: index(0-index)
+        x: update value
+        """
+        k += self.num
+        self.tree[k] = x
+        while k > 1:
+            self.tree[k >> 1] = self.segfunc(self.tree[k], self.tree[k ^ 1])
+            k >>= 1
+
+    def query(self, l, r):
+        """
+        [l, r)のsegfuncしたものを得る
+        l: index(0-index)
+        r: index(0-index)
+        """
+        res = self.ide_ele
+
+        l += self.num
+        r += self.num
+        while l < r:
+            if l & 1:
+                res = self.segfunc(res, self.tree[l])
+                l += 1
+            if r & 1:
+                res = self.segfunc(res, self.tree[r - 1])
+            l >>= 1
+            r >>= 1
+        return res
+
+def main():
+    N, Q = mapint()
+    S = lstr()
+    
+    arr = [0]*N
+    for i in range(N-1):
+        if S[i] != S[i+1]:
+            arr[i] = 1
+    sgTree = SegTree(arr, segfunc, ide_ele)
+    # errprint(arr)
+    for _ in range(Q):
+        t, l, r = mapint0()
+        if t == 0:
+            if l != 0:
+                v = sgTree.query(l-1, l)
+                sgTree.update(l-1, v^1)
+            if r != N:
+                v = sgTree.query(r, r+1)
+                sgTree.update(r, v^1)
+            # errprint(sgTree.tree[sgTree.num:sgTree.num+N])
+        else:
+            v = sgTree.query(l, r)
+            print("No" if v == 0 else "Yes")
+            
+    
+    
+
+def ini(): return int(input())
+def mapint(): return map(int, input().split())
+def mapint0(): return map(lambda x: int(x)-1, input().split())
+def mapstr(): return input().split()
+def lint(): return list(map(int, input().split()))
+def lint0(): return list(map(lambda x: int(x)-1, input().split()))
+def lstr(): return list(input().rstrip())
+def errprint(*x): return None if atcenv else print(*x, file=sys.stderr) 
+
+if __name__=="__main__":
+    import sys, os
+    input = sys.stdin.readline
+    atcenv = os.environ.get("ATCODER", 0)
+
+    main()
