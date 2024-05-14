4475b72a45743b73b0ab985afb59bbceb008d744
yatoyun <kaneko.yuta0813@gmail.com>
2024-05-14 20:23:58 +0900

    :white_check_mark: 10/38 another algorithm minutes :green_square:

--- 276/e.py
+++ 276/e.py
@@ -1,27 +1,172 @@
+from collections import defaultdict
+
+class UnionFind():
+    """
+    Union Find木クラス
+
+    Attributes
+    --------------------
+    n : int
+        要素数
+    root : list
+        木の要素数
+        0未満であればそのノードが根であり、添字の値が要素数
+    rank : list
+        木の深さ
+    """
+
+    def __init__(self, n):
+        """
+        Parameters
+        ---------------------
+        n : int
+            要素数
+        """
+        self.n = n
+        self.root = [-1]*(n+1)
+        self.rank = [0]*(n+1)
+
+    def find(self, x):
+        """
+        ノードxの根を見つける
+
+        Parameters
+        ---------------------
+        x : int
+            見つけるノード
+
+        Returns
+        ---------------------
+        root : int
+            根のノード
+        """
+        if(self.root[x] < 0):
+            return x
+        else:
+            self.root[x] = self.find(self.root[x])
+            return self.root[x]
+
+    def unite(self, x, y):
+        """
+        木の併合
+
+        Parameters
+        ---------------------
+        x : int
+            併合したノード
+        y : int
+            併合したノード
+        """
+        x = self.find(x)
+        y = self.find(y)
+
+        if(x == y):
+            return
+        elif(self.rank[x] > self.rank[y]):
+            self.root[x] += self.root[y]
+            self.root[y] = x
+        else:
+            self.root[y] += self.root[x]
+            self.root[x] = y
+            if(self.rank[x] == self.rank[y]):
+                self.rank[y] += 1
+
+    def same(self, x, y):
+        """
+        同じグループに属するか判定
+
+        Parameters
+        ---------------------
+        x : int
+            判定したノード
+        y : int
+            判定したノード
+
+        Returns
+        ---------------------
+        ans : bool
+            同じグループに属しているか
+        """
+        return self.find(x) == self.find(y)
+
+    def size(self, x):
+        """
+        木のサイズを計算
+
+        Parameters
+        ---------------------
+        x : int
+            計算したい木のノード
+
+        Returns
+        ---------------------
+        size : int
+            木のサイズ
+        """
+        return -self.root[self.find(x)]
+
+    def roots(self):
+        """
+        根のノードを取得
+
+        Returns
+        ---------------------
+        roots : list
+            根のノード
+        """
+        return [i for i, x in enumerate(self.root) if x < 0]
+
+    def group_size(self):
+        """
+        グループ数を取得
+
+        Returns
+        ---------------------
+        size : int
+            グループ数
+        """
+        return len(self.roots())
+
+    def group_members(self):
+        """
+        全てのグループごとのノードを取得
+
+        Returns
+        ---------------------
+        group_members : defaultdict
+            根をキーとしたノードのリスト
+        """
+        group_members = defaultdict(list)
+        for member in range(self.n):
+            group_members[self.find(member)].append(member)
+        return group_members
+    
 def main():
     H, W = mapint()
     
     C = [lstr() for _ in range(H)]
     
+    uf = UnionFind(H*W-1)
     for i in range(H):
         for j in range(W):
             if C[i][j] == "S":
                 st = (i, j)
-    
-    visited = set()
-    stack = [(0, st)] # cost, node
-    while stack:
-        cost, node = stack.pop()
-        if node in visited:
-            if node == st and cost >= 4:
+            elif C[i][j] == ".":
+                for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                    nx, ny = i+dx, j+dy
+                    if 0 <= nx < H and 0 <= ny < W and C[nx][ny] != "#" and C[nx][ny] != "S":
+                        uf.unite(i*W+j, nx*W+ny)
+    adjacents = []
+    for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+        if 0 <= st[0]+dx < H and 0 <= st[1]+dy < W and C[st[0]+dx][st[1]+dy] != "#":
+            adjacents.append((st[0]+dx, st[1]+dy))
+
+    for i in range(len(adjacents)-1):
+        for j in range(i+1, len(adjacents)):
+            if uf.same(adjacents[i][0]*W+adjacents[i][1], adjacents[j][0]*W+adjacents[j][1]):
                 print("Yes")
-                exit()
-            continue
-        visited.add(node)
-        i, j = node
-        for ni, nj in [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]:
-            if 0 <= ni < H and 0 <= nj < W and C[ni][nj] != "#":
-                stack.append((cost+1, (ni, nj)))
+                return
+    
     print("No")
 
 def ini(): return int(input())
