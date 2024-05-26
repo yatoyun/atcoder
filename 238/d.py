16d631543a03a074575c1697073affc76e796735
yatoyun <kaneko.yuta0813@gmail.com>
2024-05-26 20:35:28 +0900

    :rotating_light: need retry can't solve :green_square:

---
+++ 238/d.py
@@ -0,0 +1,27 @@
+def main():
+    T = ini()
+    
+    for _ in range(T):
+        a, s = mapint()
+        if 2*a > s:
+            print("No")
+            continue
+        if (s-2*a) & a == 0:
+            print("Yes")
+        else:
+            print("No")
+
+def ini(): return int(input())
+def mapint(): return map(int, input().split())
+def mapint0(): return map(lambda x: int(x)-1, input().split())
+def mapstr(): return input().split()
+def lint(): return list(map(int, input().split()))
+def lint0(): return list(map(lambda x: int(x)-1, input().split()))
+def lstr(): return list(input().rstrip())
+def errprint(*x): return None if atcenv else print(*x, file=sys.stderr) 
+
+if __name__=="__main__":
+    import sys, os
+    input = sys.stdin.readline
+    atcenv = os.environ.get("ATCODER", 0)
+    main()
